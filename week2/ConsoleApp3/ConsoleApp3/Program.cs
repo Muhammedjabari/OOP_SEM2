c10c2005a4fe76a8ec296068544f83940c81de62
101668629 <101668629@student.swin.edu.au>
2018-08-06 15:04:47 +1000

    initial commit

---
+++ week2/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        // Function to check perfect square
+        static bool isSquare(int n)
+        {
+            int sr = (int)Math.Sqrt(n);
+
+            return (sr * sr == n);
+        }
+        static void printPrimeAndFib(int n)
+        {
+            bool[] prime = new bool[n + 1];
+
+
+            for (int p = 0; p <= n; p++)
+                prime[p] = true;
+
+            for (int p = 2; p * p <= n; p++)
+            {
+                if (prime[p] == true)
+                {
+                    for (int i = p * 2; i <= n; i += p)
+                        prime[i] = false;
+                }
+            }
+
+        }
+    }
+}
